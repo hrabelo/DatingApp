119eee9c6124e8bcfb91a4ac5e5f068ee60d11ec
Hian Castilho <hian_castilho@poli.ufrj.br>
2020-09-21 22:33:50 -0300

    :new: Includes a global exception handler.

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,8 +1,13 @@
+using System.Data.Common;
+using System.Net;
 using System.Text;
 using DatingApp.API.Data;
+using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -45,9 +50,35 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
+
+                app.UseExceptionHandler(builder => 
+                {
+                    builder.Run(async context => {
+                        
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        
+                        var exception = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if (exception != null) 
+                        {
+                            if (exception.Error is DbException)
+                            {
+                                string message = "Our database server is offline. Please, try again later.";
+                                context.Response.AddApplicationError(message);
+                                await context.Response.WriteAsync(message);
+                            }
+                            else
+                            {
+                                context.Response.AddApplicationError(exception.Error.Message);
+                                await context.Response.WriteAsync(exception.Error.Message);
+                            }
+                        }
+                    });
+                });
             }
 
             // app.UseHttpsRedirection();
