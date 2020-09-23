b05de8f84f046f521f1f3d109dfb4432b031b2c6
Hian Castilho <hian_castilho@poli.ufrj.br>
2020-09-22 21:29:55 -0300

    :new: Includes the ErrorInterceptorProvider logic.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
       NavComponent,
       HomeComponent,
@@ -23,7 +24,8 @@ import { RegisterComponent } from './register/register.component';
     FormsModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    ErrorInterceptorProvider
   ],
   bootstrap: [AppComponent]
 })
